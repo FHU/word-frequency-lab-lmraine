f1edd9bbeb8c3b1c02822cda018cc0364b65d950
lmraine <lraine@fhu.edu>
2024-01-31 09:08:08 -0600

    main.py
    updated function

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def build_dictionary(words):
-    pass
+    word_counts = {}
+    for word in words:
+        if word in word_counts.keys():
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    return word_counts
 
 # REMOVE PASS AND FIX THE CODE TO:
 #1. asks for input, 
@@ -10,4 +16,7 @@ def build_dictionary(words):
 if __name__ == '__main__':
     user_input = input("Enter your input: ")
     words = user_input.split()
+    word_counts = build_dictionary(words)
+    for key in sorted(word_counts.keys()):
+        print(key,"-",word_counts[key])
     
