52b0fc9ef83cf2b00a26b41d8f4ea418eed2eef5
lmraine <lraine@fhu.edu>
2024-01-31 09:16:59 -0600

    main.py
    remove comments for demo

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ def build_dictionary(words):
     return word_counts
 
 # REMOVE PASS AND FIX THE CODE TO:
-#1. asks for input, 
-#2. splits the input into a word list
-#3. calls build_dictionary() that will return a dictionary of each word in the input and it's frequency
-#4. output the contents of the dictionary sorted by key.
 if __name__ == '__main__':
     user_input = input("Enter your input: ")
     words = user_input.split()
