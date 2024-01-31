e6ef5e81850e6ea7d4cea5dfabb495fafa46abd3
lmraine <lraine@fhu.edu>
2024-01-31 09:02:54 -0600

    main.py
    demo lab

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ def build_dictionary(words):
 #3. calls build_dictionary() that will return a dictionary of each word in the input and it's frequency
 #4. output the contents of the dictionary sorted by key.
 if __name__ == '__main__':
-    pass
+    user_input = input("Enter your input: ")
+    words = user_input.split()
+    
